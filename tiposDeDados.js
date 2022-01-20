001edaaa3b2d301651558cf3f5f635088f269858
FabioCrippa <crippa.fabiocrippa@gmail.com>
2022-01-20 11:21:12 -0300

    primeiro commit

---
+++ tiposDeDados.js
@@ -0,0 +1,52 @@
+array
+//let seriesFavoristas = ["Friends", "Breaking Bad"]
+//console.log(series);
+
+//let numeroSorteados = [12, 45, 56, 324, 452]
+//numerosSorteados[0]----12
+//numerosSorteados[1]----45
+
+
+objeto literal
+//let meuCarro = {
+    //marca: "fiat", (propriedade marca)
+    //ano: 2020 (propriedade ano)
+//}
+//console.log(meuCarro);
+
+undefined valor nao definido
+//let espectadores = undefined;
+//espectadores = 10; neste caso, agora imprime o valor 10. Agora sim temos um valor
+//console.log(espectadores);
+
+
+null que nao foi definido
+//let jogadores = Null; Nao chegou um dado, algo falhou
+//console.log(jogadores); 
+
+NaN Not a number
+//let naoNumerico = NaN;
+//console.log(naoNumerico);
+
+//let divisaoRuim = "35" / 2; NaN nao e um numero
+
+boolean
+//let vouParaPraia = false;
+//console.log(vouParaPraia);
+//false
+
+//let diaEstaChuvoso = true;
+//console.log(diaEstaChuvoso);
+
+decimal
+//let altura = 1.70;
+//console.log(idade);
+
+inteiro
+//let idade = 39;
+//console.log(idade);
+
+
+string
+//let nome = "Fabio";
+//console.log("nome");
